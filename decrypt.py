ddab97bb570183da0642e64d933ed45d42d7c36a
Shankhosuvro Ghosh <98182979+Shankhosuvro-G@users.noreply.github.com>
2024-05-03 18:10:40 +0530

    Create decrypt.py

---
+++ decrypt.py
@@ -0,0 +1,20 @@
+import  os
+from cryptography.fernet import Fernet
+
+
+files=[]
+for file in os.listdir():
+        if file == "voldemort.py" or file == "thekey.key" or file == "decrypt.py":
+                continue
+        if os.path.isfile(file):
+                files.append(file)
+print(files)
+
+with open("thekey.key", "wb") as thekey:
+        thekey.read()
+for file in files:
+        with open(file, "rb") as thefile:
+                contents=thefile.read()
+        contents_decrypted = Fernet(key).decrypt(contents)
+        with open(file,"wb") as thefile:
+                thefile.write(contents_decrypted)
